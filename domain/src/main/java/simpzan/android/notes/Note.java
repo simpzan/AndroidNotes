9bd54e7119ad422c3b51a2a2749e72447be46614
simpzan <simpzan@gmail.com>
2014-10-22 18:46:08 +0800

    add model class Note.

--- domain/src/main/java/simpzan/android/notes/Note.java
+++ domain/src/main/java/simpzan/android/notes/Note.java
@@ -1,4 +1,47 @@
 package simpzan.android.notes;
 
+import java.util.Date;
+
 public class Note {
+    private String title;
+    private String content;
+    private Date modified;
+    private long id;
+
+    public Note(String title) {
+        this.title = title;
+        this.modified = new Date();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getModified() {
+        return modified;
+    }
+
+    public void setModified(Date modified) {
+        this.modified = modified;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
--- domain/src/test/java/simpzan/android/notes/NoteTest.java
+++ domain/src/test/java/simpzan/android/notes/NoteTest.java
@@ -1,7 +1,25 @@
 package simpzan.android.notes;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 public class NoteTest {
+    @Test
+    public void createNoteTest() {
+        String title = "title_test";
+        Date now = new Date();
 
+        Note note = new Note(title);
+        Assert.assertEquals(note.getTitle(), title);
+        assertDatesAlmostEqual(note.getModified(), now);
+    }
+
+    private void assertDatesAlmostEqual(Date expected, Date actual) {
+        long diff = expected.getTime() - actual.getTime();
+        Assert.assertTrue(Math.abs(diff) < 10);
+    }
 }
