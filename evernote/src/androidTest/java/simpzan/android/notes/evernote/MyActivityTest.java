64120055971f9ea66a95961e5317966edcd58d47
simpzan <simpzan@gmail.com>
2014-11-01 21:13:39 +0800

    add robolectric JVM android unit test library.

---
+++ evernote/src/androidTest/java/simpzan/android/notes/evernote/MyActivityTest.java
@@ -0,0 +1,19 @@
+package simpzan.android.notes.evernote;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+
+@RunWith(RobolectricTestRunner.class)
+public class MyActivityTest {
+
+    @Test
+    public void shouldHaveHappySmiles() throws Exception {
+//        assertThat("", equalTo("Hello World!"));
+    }
+
+}
